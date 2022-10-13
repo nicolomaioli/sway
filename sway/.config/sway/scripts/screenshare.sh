68f02623ec2ca88f78ca25fdebbaa0369142b7f3
Nicolo Maioli <nicolomaioli@gmail.com>
2022-10-13 09:09:03 +0100

    Correctly set WAYLAND_DISPLAY

---
+++ sway/.config/sway/scripts/screenshare.sh
@@ -0,0 +1,7 @@
+#!/bin/bash
+
+# See https://github.com/emersion/xdg-desktop-portal-wlr/wiki/%22It-doesn't-work%22-Troubleshooting-Checklist
+dbus-update-activation-environment --systemd WAYLAND_DISPLAY XDG_CURRENT_DESKTOP=sway
+systemctl --user stop pipewire wireplumber xdg-desktop-portal xdg-desktop-portal-wlr
+systemctl --user start wireplumber
+systemctl --user start pipewire xdg-desktop-portal xdg-desktop-portal-wlr
