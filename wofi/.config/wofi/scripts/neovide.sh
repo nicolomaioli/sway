e31eec1c88f4bf9c2b5103a1647561c948352bbc
Nicolo Maioli <nicolomaioli@gmail.com>
2022-10-23 22:24:46 +0100

    Add neovim launcher

---
+++ wofi/.config/wofi/scripts/neovide.sh
@@ -0,0 +1,10 @@
+#!/bin/zsh
+
+TARGET=$(find ~/Code ~/.config -mindepth 1 -maxdepth 1 -type d | wofi -d -p system)
+
+if [ -n "$TARGET" ]; then
+	APP_ID=$(echo $RANDOM)
+	BASEPATH=${TARGET##*/}
+	neovide $TARGET --wayland-app-id $APP_ID
+	swaymsg "for_window [app_id=$APP_ID] title_format 'Neovide - $BASEPATH'"
+fi
